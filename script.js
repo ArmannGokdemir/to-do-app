9db6b45fe7243c0e4c074cba79428fdac34ff391
ArmannGokdemir <armangokde@gmail.com>
2023-11-11 18:13:46 +0300

    deleting tasks

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ const btnAdd = document.querySelector(".add-task");
 const taskInput = document.getElementById("task-input");
 const list = document.querySelector(".tasks");
 
-let taskList = [];
+let taskList = new Map();
 
 const init = function () {
-  taskList = [...localStorage.getItem("tasks").split(",")];
-  for (const task of taskList) {
+  // localStorage.clear();
+  taskList = new Map(JSON.parse(localStorage.getItem("tasks"))) || new Map();
+  // console.log("task list ", taskList.get("abc"));
+  // [...localStorage.getItem("tasks").split(",")].map((item) => item);
+  for (const [task] of taskList) {
     const li = document.createElement("li");
     li.classList.add("task-item");
     li.textContent = task;
@@ -22,15 +25,24 @@ const addEvent = function () {
     const li = document.createElement("li");
     li.textContent = taskInput.value;
     list.append(li);
-    taskList.push(taskInput.value);
+    taskList.set(taskInput.value, taskInput.value);
     // add task to local storage
-    localStorage.setItem("tasks", taskList);
+    localStorage.setItem(
+      "tasks",
+      JSON.stringify(Array.from(taskList.entries()))
+    );
+
     taskInput.value = "";
   } else {
     alert("Task name cant be empty");
   }
 };
 const deleteTask = function () {
+  taskList.delete(
+    this.parentNode.textContent.slice(0, this.parentNode.textContent.length - 1)
+  );
+
+  localStorage.setItem("tasks", JSON.stringify(Array.from(taskList.entries())));
   this.parentNode.remove();
 };
 
